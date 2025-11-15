3c45272a31e90c5a441c52725f9000f4d354bfbf
TriLuu <triluu2109@gamil.com>
2025-11-15 19:54:23 +0700

    test

--- OneEE/main.py
+++ OneEE/main.py
@@ -153,39 +153,46 @@ class Trainer(object):
             
             if (i + 1) % 8 == 0:
                 with torch.no_grad():
-                    tri_pred = tri_logits.argmax(dim=-1)       # [B, L, L]
-                    arg_pred = arg_logits.argmax(dim=-1)       # [B, L, L]
-                    role_pred = role_logits.argmax(dim=-1)     # [B, L, L]
+                    tri_pred_mask = tri_logits > 0
+                    arg_pred_mask = arg_logits > 0
+                    role_pred_mask = role_logits > 0
 
-                    tri_gold = tri_labels.long().argmax(dim=-1)
-                    arg_gold = arg_labels.long().argmax(dim=-1)
-                    role_gold = role_labels.long().argmax(dim=-1)
+                    tri_gold_mask = tri_labels
+                    arg_gold_mask = arg_labels
+                    role_gold_mask = role_labels
 
-                    sample_b = 0
-                    sample_words = min(5, tri_pred.size(1))
+                    tri_pred_idx = tri_pred_mask.nonzero(as_tuple=False)
+                    tri_gold_idx = tri_gold_mask.nonzero(as_tuple=False)
+
+                    arg_pred_idx = arg_pred_mask.nonzero(as_tuple=False)
+                    arg_gold_idx = arg_gold_mask.nonzero(as_tuple=False)
+
+                    role_pred_idx = role_pred_mask.nonzero(as_tuple=False)
+                    role_gold_idx = role_gold_mask.nonzero(as_tuple=False)
 
                     logger.info(
-                        "Epoch {} | Batch {} | tri_pred {} | tri_gold {}".format(
+                        "Epoch {} | Batch {} | tri_pred(first 20) {} | tri_gold(first 20) {}".format(
                             epoch,
                             i + 1,
-                            tri_pred[sample_b, :sample_words, :sample_words].tolist(),
-                            tri_gold[sample_b, :sample_words, :sample_words].tolist()
+                            tri_pred_idx[:20].tolist(),
+                            tri_gold_idx[:20].tolist()
                         )
                     )
 
                     logger.info(
-                        "arg_pred {} | arg_gold {}".format(
-                            arg_pred[sample_b, :sample_words, :sample_words].tolist(),
-                            arg_gold[sample_b, :sample_words, :sample_words].tolist()
+                        "arg_pred(first 20) {} | arg_gold(first 20) {}".format(
+                            arg_pred_idx[:20].tolist(),
+                            arg_gold_idx[:20].tolist()
                         )
                     )
 
                     logger.info(
-                        "role_pred {} | role_gold {}".format(
-                            role_pred[sample_b, :sample_words, :sample_words].tolist(),
-                            role_gold[sample_b, :sample_words, :sample_words].tolist()
+                        "role_pred(first 20) {} | role_gold(first 20) {}".format(
+                            role_pred_idx[:20].tolist(),
+                            role_gold_idx[:20].tolist()
                         )
                     )
+
 
 
 
